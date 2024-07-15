0890ec7b8a8d8e8508568a011594de572fca2141
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-07-15 11:23:28 +0300

    Debugging

--- api/v1/routes/guests.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""A module for viewing Guest objects."""
-from api.v1.routes import app_views
-from flask import jsonify
-
-
-#@app_views.route()
--- api/v1/routes/tasks.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""A module for viewing Task objects."""
-from api.v1.routes import app_views
-from flask import jsonify
-
-
